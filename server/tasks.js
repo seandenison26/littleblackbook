1701ddf22df67572286adb00147409b94a6eac81
unknown <sean.denison@gmail.com>
2018-01-09 21:56:28 -0600

    wrote queryDB function

--- server/tasks.js
+++ server/tasks.js
@@ -4,10 +4,36 @@ const DB = '127.0.0.1:5984/lbb_dev'
 
 //desired middle ware, 
 
+//performs a get request to the DB and returns a JSON view
+const queryDB = (url) => {
+	return new Promise((res,rej) => {
+		http.get(url, (view) => {
+			let rawData = '' 
+			view.setEncoding('utf8')
+			view.on('data', c => rawData += c)
+			view.on('end',() => { 	
+			res(JSON.parse(rawData))
+			})
+			view.on('error', (e) => rej(e))	
+		})
+	})
+}
+
 
 //takes in an array, gets number of UUIDs based upon the length and adds an _id prop to each of the objs
-const addUUID = (objs) = {
-	
+const addUUID = (objs) => {
+	var url = `http:\/\/127.0.0.1:5984/_uuids?count=${objs.length}`
+	return new Promise((res,rej) => {
+		http.get(url, (view) => {
+			let rawData = '', ids, json
+			view.setEncoding('utf8')
+			view.on('data', c => rawData += c)
+			view.on('end',() => { 	
+			json = JSON.parse(rawData)
+			res(json)
+			})
+		})
+	})	
 }
 
 //gets all userVen from user id
