79f2698821f627fb3cd4521e6414d9b6a9490cf3
unknown <sean.denison@gmail.com>
2018-01-09 22:50:29 -0600

    addUUID succesfully adds uuids to an array of objects

--- server/router.js
+++ server/router.js
@@ -14,16 +14,27 @@ router.get('/', function(req, res) {
 	res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
-
 //returns an array of the users ven from the db, call backmiddle ware will probablly authticate, or build an object, get uuid is probablly a good one as well
 router.get('/api/getUserVen/:user/', (req, res) => {
-	let user = req.params.user;
+	let user = req.params.user
 	tasks.getUserVen(user)
 		.then((data) => {
+			console.log(data)
 			res.send(data)
 		})
 		.catch((err) => res.send(new Error ("User has no Ven.")));
 });
 
+router.post('/api/getUUIDs', (req, res) => {
+	tasks.addUUID(req.body)
+		.then((data) => {
+			res.send(req.body)
+		})
+		.catch((err) => res.send(err));
+
+});
+
+//exports the router
+module.exports = router; 
 //exports the router
 module.exports = router; 
--- server/tasks.js
+++ server/tasks.js
@@ -1,7 +1,6 @@
 const http = require('http')
 const DB = '127.0.0.1:5984/lbb_dev'
 
-
 //desired middle ware, 
 
 //performs a get request to the DB and returns a JSON view
@@ -19,21 +18,16 @@ const queryDB = (url) => {
 	})
 }
 
-
 //takes in an array, gets number of UUIDs based upon the length and adds an _id prop to each of the objs
 const addUUID = (objs) => {
 	var url = `http:\/\/127.0.0.1:5984/_uuids?count=${objs.length}`
 	return new Promise((res,rej) => {
-		http.get(url, (view) => {
-			let rawData = '', ids, json
-			view.setEncoding('utf8')
-			view.on('data', c => rawData += c)
-			view.on('end',() => { 	
-			json = JSON.parse(rawData)
-			res(json)
+		queryDB(url)
+			.then((ids) => {
+				res(objs.map((o,i) => Object.assign(o,{"_id":ids.uuids[i]})))	
 			})
-		})
-	})	
+			.catch(rej)		
+	})
 }
 
 //gets all userVen from user id
